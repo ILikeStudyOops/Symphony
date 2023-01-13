4613c4e7bcc34e52846f87cf51f86ad7dbb6bd32
‘caoshengqiang’ <csq@123.com>
2023-01-13 12:46:34 +0800

    启动项目出现Lombok不生效的问题，将Lombok依赖从夫工程公共依赖转移到了common模块中，仍然存在该问题，最终只好手动添加了set、get方法

--- common/src/main/java/org/csq/entity/Order.java
+++ common/src/main/java/org/csq/entity/Order.java
@@ -7,4 +7,32 @@ public class Order {
     private Integer id;
     private Double price;
     private User user;
+
+    /**
+     * Order和User都存在Lombok不生效的情况，因此无奈增加了set、get方法
+     * @return
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
--- common/src/main/java/org/csq/entity/User.java
+++ common/src/main/java/org/csq/entity/User.java
@@ -7,4 +7,32 @@ public class User {
     private Integer id;
     private String username;
     private String password;
+
+    /**
+     * Order和User都存在Lombok不生效的情况，因此无奈增加了set、get方法
+     * @return
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
