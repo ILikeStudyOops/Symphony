74cc26ddad451b9d7227bd3b86332acc5cc875e1
caoshengqiang <2128459108@qq.com>
2023-09-17 17:15:01 +0800

    新增加es模块，继续完善基本代码及测试工具类

--- elasticsearch/src/main/java/org/csq/util/EsUtil.java
+++ elasticsearch/src/main/java/org/csq/util/EsUtil.java
@@ -29,13 +29,18 @@ import org.elasticsearch.client.indices.GetIndexResponse;
 import org.elasticsearch.cluster.metadata.AliasMetadata;
 import org.elasticsearch.cluster.metadata.MappingMetadata;
 import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.unit.Fuzziness;
 import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.index.get.GetResult;
-import org.elasticsearch.index.query.QueryBuilder;
-import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.query.*;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.aggregations.AggregationBuilder;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.aggregations.bucket.adjacency.AdjacencyMatrixAggregationBuilder;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
+import org.elasticsearch.search.sort.SortOrder;
 import org.springframework.util.ObjectUtils;
 
 import java.io.IOException;
@@ -74,11 +79,11 @@ public class EsUtil {
         }
 
         //更新数据
-        Map<String,String> updateData = new HashMap<>();
+        /*Map<String,String> updateData = new HashMap<>();
         updateData.put("name","rose");
         updateData.put("sex","女");
         updateData.put("tel","13922222222");
-        GetResult res = updateData(client, "spring", "1002", updateData);
+        GetResult res = updateData(client, "spring", "1002", updateData);*/
 
         //查询数据
         GetResponse getResponse = getData(client,"spring","1001");
@@ -98,6 +103,196 @@ public class EsUtil {
         System.out.println("数据条数：" + hits.getTotalHits());
         for (SearchHit hit : hits) {
             System.out.println(hit.getSourceAsString());
+        }
+
+        //条件查询
+        /*SearchRequest conditionalQueryRequest = new SearchRequest();
+        conditionalQueryRequest.indices("spring");
+        conditionalQueryRequest.source(new SearchSourceBuilder().query(QueryBuilders.termQuery("sex","男")));
+        SearchResponse search;
+        try {
+            search = client.search(conditionalQueryRequest, RequestOptions.DEFAULT);
+            SearchHits hits1 = search.getHits();
+            for (SearchHit hit : hits1) {
+                System.out.println(hit);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+        //条件查询带分页
+        /*SearchRequest conditionalQueryRequest = new SearchRequest();
+        conditionalQueryRequest.indices("spring");
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder().query(QueryBuilders.matchAllQuery());
+        searchSourceBuilder.from(0);
+        searchSourceBuilder.size(2);
+        conditionalQueryRequest.source(searchSourceBuilder);
+        SearchResponse search;
+        try {
+            search = client.search(conditionalQueryRequest, RequestOptions.DEFAULT);
+            SearchHits hits1 = search.getHits();
+            for (SearchHit hit : hits1) {
+                System.out.println(hit);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+
+        /*//条件查询带排序
+        SearchRequest conditionalQueryRequest = new SearchRequest();
+        conditionalQueryRequest.indices("spring");
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder().query(QueryBuilders.matchAllQuery());
+        searchSourceBuilder.sort("age", SortOrder.DESC);
+        conditionalQueryRequest.source(searchSourceBuilder);
+        SearchResponse search;
+        try {
+            search = client.search(conditionalQueryRequest, RequestOptions.DEFAULT);
+            SearchHits hits1 = search.getHits();
+            for (SearchHit hit : hits1) {
+                System.out.println(hit);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+        //条件查询带过滤字段
+        /*SearchRequest conditionalQueryRequest = new SearchRequest();
+        conditionalQueryRequest.indices("spring");
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder().query(QueryBuilders.matchAllQuery());
+        String[] includes = {"name"};
+        String[] excludes = {};
+        searchSourceBuilder.fetchSource(includes,excludes);
+        conditionalQueryRequest.source(searchSourceBuilder);
+        SearchResponse search;
+        try {
+            search = client.search(conditionalQueryRequest, RequestOptions.DEFAULT);
+            SearchHits hits1 = search.getHits();
+            for (SearchHit hit : hits1) {
+                System.out.println(hit);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+        //组合条件查询
+        /*SearchRequest conditionalQueryRequest = new SearchRequest();
+        conditionalQueryRequest.indices("spring");
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery();
+        //设置条件
+        *//*boolQueryBuilder.must(QueryBuilders.matchQuery("name","zhangsan"));
+        boolQueryBuilder.must(QueryBuilders.matchQuery("sex","男"));*//*
+        boolQueryBuilder.should(QueryBuilders.matchQuery("name","zhangsan"));
+        boolQueryBuilder.should(QueryBuilders.matchQuery("name","lisi"));
+        searchSourceBuilder.query(boolQueryBuilder);
+        conditionalQueryRequest.source(searchSourceBuilder);
+        SearchResponse search;
+        try {
+            search = client.search(conditionalQueryRequest, RequestOptions.DEFAULT);
+            SearchHits hits1 = search.getHits();
+            for (SearchHit hit : hits1) {
+                System.out.println(hit);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+
+        //范围查询
+        //组合条件查询
+        /*SearchRequest conditionalQueryRequest2 = new SearchRequest();
+        conditionalQueryRequest2.indices("spring");
+        SearchSourceBuilder searchSourceBuilder2 = new SearchSourceBuilder();
+        RangeQueryBuilder rangeQueryBuilder = QueryBuilders.rangeQuery("age");
+        //设置条件
+        rangeQueryBuilder.gte(30);
+        rangeQueryBuilder.lte(40);
+        searchSourceBuilder2.query(rangeQueryBuilder);
+        conditionalQueryRequest2.source(searchSourceBuilder2);
+        SearchResponse search2;
+        try {
+            search2 = client.search(conditionalQueryRequest2, RequestOptions.DEFAULT);
+            SearchHits hits1 = search2.getHits();
+            for (SearchHit hit : hits1) {
+                System.out.println(hit);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+
+        //模糊查询
+        /*SearchRequest conditionalQueryRequest3 = new SearchRequest();
+        conditionalQueryRequest3.indices("spring");
+        SearchSourceBuilder searchSourceBuilder3 = new SearchSourceBuilder();
+        searchSourceBuilder3.query(QueryBuilders.fuzzyQuery("name","zhangsan").fuzziness(Fuzziness.ONE));
+        conditionalQueryRequest3.source(searchSourceBuilder3);
+        SearchResponse search3;
+        try {
+            search3 = client.search(conditionalQueryRequest2, RequestOptions.DEFAULT);
+            SearchHits hits1 = search3.getHits();
+            for (SearchHit hit : hits1) {
+                System.out.println(hit);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+
+        //高亮查询
+        /*SearchRequest conditionalQueryRequest4 = new SearchRequest();
+        conditionalQueryRequest4.indices("spring");
+        SearchSourceBuilder searchSourceBuilder4 = new SearchSourceBuilder();
+        TermsQueryBuilder termsQueryBuilder = QueryBuilders.termsQuery("name", "lisi");
+        HighlightBuilder highlightBuilder = new HighlightBuilder();
+        highlightBuilder.preTags("<font color='red'>");
+        highlightBuilder.postTags("</font>");
+        highlightBuilder.field("name");
+        searchSourceBuilder4.highlighter(highlightBuilder);
+        searchSourceBuilder4.query(termsQueryBuilder);
+        conditionalQueryRequest4.source(searchSourceBuilder4);
+        SearchResponse search4;
+        try {
+            search4 = client.search(conditionalQueryRequest4, RequestOptions.DEFAULT);
+            SearchHits hits1 = search4.getHits();
+            for (SearchHit hit : hits1) {
+                System.out.println(hit.getSourceAsString());
+                System.out.println(hit.getHighlightFields());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+
+        //聚合查询
+        /*SearchRequest conditionalQueryRequest5 = new SearchRequest();
+        conditionalQueryRequest5.indices("spring");
+        SearchSourceBuilder searchSourceBuilder5 = new SearchSourceBuilder();
+        AggregationBuilder aggregationBuilder = AggregationBuilders.max("maxAge").field("age");
+        searchSourceBuilder5.aggregation(aggregationBuilder);
+
+        conditionalQueryRequest5.source(searchSourceBuilder5);
+        SearchResponse search5;
+        try {
+            search5 = client.search(conditionalQueryRequest5, RequestOptions.DEFAULT);
+            SearchHits hits1 = search5.getHits();
+            for (SearchHit hit : hits1) {
+                System.out.println(hit.getSourceAsString());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+
+        //分组查询
+        SearchRequest conditionalQueryRequest5 = new SearchRequest();
+        conditionalQueryRequest5.indices("spring");
+        SearchSourceBuilder searchSourceBuilder5 = new SearchSourceBuilder();
+        AggregationBuilder aggregationBuilder = AggregationBuilders.terms("nameGroup").field("name");
+        searchSourceBuilder5.aggregation(aggregationBuilder);
+
+        conditionalQueryRequest5.source(searchSourceBuilder5);
+        SearchResponse search5;
+        try {
+            search5 = client.search(conditionalQueryRequest5, RequestOptions.DEFAULT);
+            SearchHits hits1 = search5.getHits();
+            for (SearchHit hit : hits1) {
+                System.out.println(hit.getSourceAsString());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
         //删除数据
